21e453521de30ffdd45f18306bd74092f67546a8
Ansoumane conde <112602207+Ansoumane621@users.noreply.github.com>
2024-05-13 15:29:36 +0000

    Update feekback.dart

--- lib/widgets/widget_pages/feekback.dart
+++ lib/widgets/widget_pages/feekback.dart
@@ -55,7 +55,7 @@ class _FeekBackState extends State<FeekBack>
               child: Column(
                 children: [
                   InputFrom(
-                      titre: 'subjet',
+                      titre: 'sujet',
                       keyBoard: TextInputType.text,
                       hin: 'votre sujet',
                       nom: 'sujet',
@@ -64,11 +64,11 @@ class _FeekBackState extends State<FeekBack>
                     height: 25,
                   ),
                   InputFrom(
-                      titre: 'feekback',
+                      titre: 'feedback',
                       keyBoard: TextInputType.multiline,
-                      hin: 'votre feekback',
+                      hin: 'votre feedback',
                       nom: 'contenu',
-                      error: 'le feekback est obligatoire'),
+                      error: 'le feedback est obligatoire'),
                   const SizedBox(
                     height: 25,
                   ),
@@ -80,7 +80,7 @@ class _FeekBackState extends State<FeekBack>
                     },
                     textStyle: const TextStyle(fontSize: 24),
                     color: const Color(0xFF33BBC5),
-                    text: "Envoyer feekback",
+                    text: "Envoyer feedback",
                     fullWidthButton: true,
                     size: GFSize.LARGE,
                   )
